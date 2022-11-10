b84f6e45c4188edf510bded8c95794a9d627d978
JMarquee85 <joshmarcus85@gmail.com>
2022-11-09 22:11:16 -0700

    Some cuts
    Removed some unnecessary comments, added a side project py to check out that's semi related.

--- adp_aad.py
+++ adp_aad.py
@@ -49,11 +49,13 @@ try:
 	#cs.execute("show tables like '%%ADP%' in schema analytics.staging;")
 	cs.execute(sql_query)
 
-	## This would show all the available columns:
-	for record in cs:
-		print(record)
+	### This would show all the available columns:
+	#for record in cs:
+		#print(record)
 
 	# Set specific variables for certain pieces of information. 
+	# The date format currently looks like this: 'hire_date': datetime.date(2022, 4, 11)
+	# Would like to get that to a more readable and usable format. 
 	for a,b,c,d,e,f,g,h,i in cs:
 		employee_id = a
 		hire_date = b
@@ -66,9 +68,6 @@ try:
 		# Adding these assigned variables to a dictionary. 
 		employee_info = {'employee_id': employee_id, 'hire_date': hire_date, 'term_date': term_date, 'employment_status': employment_status, 'first_name': first_name, 'last_name': last_name, 'city': city, 'state': state}
 		print(employee_info)
-
-		#Next step would be to call WORKER_ID,WORKER_TERMINATION_DATE,WORKER_STATUS,WORKER_ORIGINAL_HIRE_DATE into another dictionary,
-		#Compare the two dictionaries and if a match between the employee_id number and the WORKER_IS is found, combine the two dictionaries, adding the additional variables.
 
 
 	### This may no longer be strictly necessary, handy to keep around as a way to get the information above into a 
