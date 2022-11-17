970a26f5d1b9c8e2217ba4f343dbfe7c0c79f867
JMarquee85 <joshmarcus85@gmail.com>
2022-11-17 12:40:06 -0700

    Update msgraphpull.py

--- msgraphpull.py
+++ msgraphpull.py
@@ -1,9 +1,11 @@
+# QUERYING MS GRAPH
 # Created AAD Enterprise Application called ADP-AAD Graph
 # https://towardsdatascience.com/querying-microsoft-graph-api-with-python-269118e8180c
 # client_secret_id = b22ec483-d46d-453d-b808-cdf276fc092a
 
 #import libraries
 import msal
+import requests #remove this once everything below is put into its own script. 
 
 # Create a token data dictionary to use with these requests
 app_id = '8af4e31e-7c65-4ad6-8cf0-3a7d67997947'
@@ -31,5 +33,35 @@ if not token_result:
   access_token = 'Bearer ' + token_result['access_token']
   print('New access token was acquired from Azure AD')
 
-print(access_token)
+# print(access_token)
+
+########################
+# This should be separated into another script or function later on and called in the main body of the script
+
+#import requests # this is called above for the moment. 
+
+# This is great, but I think it is limited due to pagination limits. 
+
+# GET USERS
+token = access_token
+url = 'https://graph.microsoft.com/v1.0/users?$select=displayName,userPrincipalName'
+# See this URL for selecting user properties, as in the above example:
+# https://learn.microsoft.com/en-us/graph/api/user-get?view=graph-rest-1.0&tabs=http
+headers = {
+  'Authorization': token
+}
+
+# GETTING GROUP LISTING
+# Copy access_token and specify the MS Graph API endpoint you want to call, e.g. 'https://graph.microsoft.com/v1.0/groups' to get all groups in your organization
+#token = access_token
+#url = 'https://graph.microsoft.com/v1.0/groups'
+#headers = {
+#  'Authorization': token
+#}
+
+# Make a GET request to the provided url, passing the access token in a header
+graph_result = requests.get(url=url, headers=headers)
+
+# Print the results in a JSON format
+print(graph_result.json())
 
