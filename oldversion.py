26f3719308cd741f39d7105673264d83f66931a4
JMarquee85 <joshmarcus85@gmail.com>
2022-11-21 11:55:32 -0700

    SQL query updates and changes
    Made significant changes to the SQL query that is pulling the information over.
    
    Removed a large section of the script that was attempting to put the information in a dictionary.
    
    Look to move forward using a pandas dataframe to store this information and work with it to compare to MS Graph.
    
    Moved old version of code for easy access to oldversion.py

--- oldversion.py
+++ oldversion.py
@@ -50,7 +50,8 @@ ctx = snowflake.connector.connect(
 cs = ctx.cursor()
 print("\nCursor object created.")
 
-# This query pulls all needed information and joins the tables together, sorting by WorkerID. 
+# This query combines the two tables with the needed information and combines them, using the worker ID number to sort. 
+# Going to need to combine this statement with some other tables in order to bring everything that we need forward. Adding some items to the notes file. 
 sql_query = '''
 SELECT w.WORKER_ID, w.WORKER_ORIGINAL_HIRE_DATE,w.WORKER_TERMINATION_DATE,w.WORKER_STATUS, 
         p.PERSON_WORKER_ID, p.PERSON_LEGAL_GIVEN_NAME, p.PERSON_LEGAL_FAMILY_NAME_1, p.PERSON_LEGAL_ADDRESS_CITY_NAME, p.PERSON_LEGAL_ADDRESS_COUNTRY_SUBDIVISION_LEVEL_1, 
@@ -79,10 +80,71 @@ try:
 
 	data = cs.fetch_pandas_all()
 
-	# Look into getting this into a pandas data frame and working with the information that way. 
 	with pandas.option_context('display.max_rows', None, 'display.max_columns', None):
 
 		print(data)
+
+	### This would show all the available columns:
+	#for record in cs:
+		#print(record)
+
+	# The date format currently looks like this: 'hire_date': datetime.date(2022, 4, 11)
+	# Would like to get that to a more readable and usable format. 
+
+	#for a,b,c,d,e,f,g,h,i in cs:
+		## instead going to append the output to lists and set the lists as the dictionary values
+		#employee_id.append(a)
+		#hire_date.append(b)
+		#term_date.append(c)
+		#employment_status.append(d)
+		#first_name.append(f)
+		#last_name.append(g)
+		#city.append(h)
+		#state.append(i)
+#		
+	## Adds the information collected above into one list. Not sure if this is necessary anymore, so may take out in review.  
+	#employee_info_lists = []
+	#employee_info_lists.append(employee_id)
+	#employee_info_lists.append(hire_date)
+	#employee_info_lists.append(term_date)
+	#employee_info_lists.append(employment_status)
+	#employee_info_lists.append(first_name)
+	#employee_info_lists.append(last_name)
+	#employee_info_lists.append(city)
+	#employee_info_lists.append(state)
+#
+#
+## STILL NEED TO PULL AND USE THE MANAGER INFORMATION FROM ADP/ SNOWFLAKE. 
+#
+	## Iterate over the lists that pulled the information, sort them into dictionaries with the employee id number as the key and a dictionary with the employee information as the value. 
+	#for a, b, c, d, e, f, g, h in zip(employee_id,hire_date,term_date,employment_status,first_name,last_name,city,state):
+		#employee_stuff = {'first_name': e, 'last_name': f, 'employment_status': d, 'hire_date': b, 'term_date': c, 'city': g, 'state': h}
+		#dict = {a: employee_stuff}
+		#employee_info.append(dict)
+#
+	## This removes duplicates. 
+	#employee_info_no_duplicates = []
+	#[employee_info_no_duplicates.append(x) for x in employee_info if x not in employee_info_no_duplicates]
+	## NOTE: There are still some duplicate employee IDs in here, but this appears to be because there are some ADP employee entries that 
+	## have two city and state entries. 
+#
+	## Testing the no duplicate list
+	#for x in (employee_info_no_duplicates):
+		#print(x)
+#
+	### Print this out. 
+	##for x in (employee_info):
+		##print(x)
+#
+	## A version of the above that utilizes lists for the dictionary value instead of a nested dictionary. 
+	##for a, b, c, d, e, f, g, h in zip(employee_id,hire_date,term_date,employment_status,first_name,last_name,city,state):
+		##employee_stuff = [e, f, b, c, d, g, h]
+		##dict = {a: employee_stuff}
+		##employee_info.append(dict)
+#
+## Okay, I think you're doing unnecessary work above. 
+## Go back to trying to get the information into a pandas data frame and manage it that way. 
+
 
 finally:
 	cs.close()
