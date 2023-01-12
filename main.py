a38f56cae94e355a20cea245ec7356a8afff1ea7
JMarquee85 <joshmarcus85@gmail.com>
2023-01-11 18:15:44 -0700

    Added a basic main.py and packaged some of the Snowflake stuff into functions. About to merge changes into main.

--- main.py
+++ main.py
@@ -3,14 +3,12 @@
 from adp_aad import * 
 from msgraphpull import * 
 
-
-
 # Authenticate Microsoft
 print("Authenticating into MS Graph...")
-ms_auth_token()
-# Pull the Microsoft user information from msgraphpull
-print(f"Now pulling AAD users via Microsoft Graph...")
 
-# Call snowflake_user_pull() 
+# Auth into Microsoft Graph API
+ms_auth_token()
+
+# Call snowflake_user_pull() and pass ms_graph_pull in as an argument to return a list of dictionaries containing the user information to compare. 
 snowflake_user_pull(ms_graph_pull())
  
