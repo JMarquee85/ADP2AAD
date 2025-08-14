f60ebd88492acefdf0072f9071d2486f49c441ba
Josh Marcus <joshmarcus85@gmail.com>
2025-08-14 12:33:48 -0600

    Delete AutoTermCheck.py

--- AutoTermCheck.py
+++
@@ -1,5 +0,0 @@
-# This is a bit of a side project, but this came to mind with some compliance and ITSec requests today. 
-
-# The goal of this script is to call the employees that have been terminated, check to see if they are terminated in AAD and, if not, run some 
-# powershell/ Graph stuff to take action on their accounts
-# as we would in a typical offboarding script. 
